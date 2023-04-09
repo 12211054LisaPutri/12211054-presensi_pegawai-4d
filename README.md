b7dff5f7b6f31b41d2272df72c7c77e1dd0aae45
12211054LisaPutri <lisa94148@gmail.com>
2023-04-09 23:37:29 +0700

    presensi pegawai

---
+++ README.md
@@ -0,0 +1,5 @@
+Nim dan nama
+12211054 : Lisa Putri
+12211146 : Julinar Widarsi
+12211188 : Desi Putri Tampubolon
+12211099 : Blandina Rika Susanti
---
+++ android/app/src/main/kotlin/id/ac/bsi/u4d_12211054/presensipegawai/MainActivity.kt
@@ -0,0 +1,6 @@
+package id.ac.bsi.u4d_12211054.presensipegawai
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
